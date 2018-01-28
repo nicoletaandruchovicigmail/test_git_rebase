3a3fa78b88fd24c901885a93f1fc453d7e48557b
nicoletaandruchovicigmail <nicoleta.andruchovici@gmail.com>
2018-01-28 14:08:31 +0100

    Added first comment to test1

--- test_git_rebase/test_git_rebase/Test1.cs
+++ test_git_rebase/test_git_rebase/Test1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace test_git_rebase
+﻿namespace test_git_rebase
 {
 	class Test1
 	{
+		// added first comment
 	}
 }
